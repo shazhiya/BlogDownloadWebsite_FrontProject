e7583905814d28ffd6026b92059b890e395dfa59
2691032513 <2691032513@qq.com>
2019-11-23 23:20:42 +0800

    完成各种配置: 跨域, axios get/post 请求参数序列化.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import qs from 'qs'
 
+const baseURL = '/api'
+
+const axiosInstance = axios.create({
+  baseURL: baseURL,
+  headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
+  // 参数序列化
+  paramsSerializer: function(params) {
+    return qs.stringify(params)
+  }
+})
+axios.defaults.baseURL = '/api'
+Vue.prototype.axios = axiosInstance
 Vue.config.productionTip = false
+Vue.prototype.qs = qs
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
