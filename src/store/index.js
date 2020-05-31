21af63515f5e21966ee641a32cbe7a004d113afb
2691032513 <2691032513@qq.com>
2020-05-31 17:09:28 +0800

    小修订

--- src/store/index.js
+++ src/store/index.js
@@ -91,13 +91,12 @@ export default new Vuex.Store({
         releaseComment(context,data){
             let type =  data.blogArticle!=null?'article':'resource'
             let axios = Vue.prototype.axios
-            if(data.parentComment.id == undefined) delete data.parentComment
-            window.console.log(data)
-            
+            if(data.parentComment.id == undefined) delete data.parentComment   
             axios
                 .post(type+'/sendComment',data)
                 .then(res=>{
-                    if (res.data) {
+                    if (res.data!=undefined && res.data!=null) {
+                        data.id = res.data
                         context.commit("releaseComment",data)
                     }else{
                         throw "失败"
